8a69d17d2b477e9c828ac4d1554fc7e4f54a4329
avazar <avazar@bk.ru>
2020-03-24 16:25:28 +0300

    Add project

---
+++ Test-path-rectangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test_path_rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример ввода
+
+            var start = new IntPoint() { X = 0, Y = 0};
+            var end = new IntPoint() { X = 113000, Y = 85000 };
+
+            var p1 = new IntPoint() { X = 37000, Y = 27000};
+            var p2 = new IntPoint() { X = 37000, Y = 67000 };
+            var p3 = new IntPoint() { X = 95000, Y = 67000 };
+            var p4 = new IntPoint() { X = 95000, Y = 27000 };
+
+            var rect = new List<IntPoint> {p1, p2, p3, p4};
+
+
+            //Результат записать в path
+            var path = new List<Segment>();
+
+        }
+    }
+}
---
+++ Test-path-rectangle/Segment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_path_rectangle
+{
+    public struct IntPoint
+    {
+        public long X;
+        public long Y;
+    }
+
+    /// <summary>
+    /// Класс, представляющий сегмент-отрезок
+    /// </summary>
+    public class Segment
+    {
+        #region START POINT
+
+        /// <summary>
+        /// Начальная точка сегмента
+        /// </summary>
+        public IntPoint StartPoint;
+
+        #endregion
+
+        #region END POINT
+
+        /// <summary>
+        /// Конечная точка сегмента
+        /// </summary>
+        public IntPoint EndPoint;
+
+        #endregion
+
+        public override string ToString()
+        {
+            return $"{this.StartPoint} - {this.EndPoint}";
+        }
+    }
+}
